0c11db4076855dd12eaf78b9b5bff765c4a85a34
V1dar <ivashchenko1919@gmail.com>
2018-07-28 21:36:30 +0300

    JavaScript for newbie, day two, homework

---
+++ JavaScriptForNewBie_day2/js/script.js
@@ -0,0 +1,34 @@
+let chooseBtn = document.getElementById("choose"),
+		reciveBtn = document.getElementById("receive"),
+		heading = document.getElementsByTagName("h2")[0],
+		nameInput = document.getElementsByClassName("contactform_name")[0],
+		phoneInput = document.querySelector(".contactform_phone"),
+		mailInput = document.querySelectorAll(".contactform_mail")[0],
+		modal = document.querySelector(".modal"),
+		buttonClose = document.querySelector(".close"),
+		text = document.getElementsByName("message")[0];
+
+function hover() {
+		heading.textContent = "Действительное всё!"
+};
+
+heading.addEventListener("click", hover);
+heading.addEventListener("dblclick", function() {
+		heading.textContent = "Все включено!"
+});
+
+reciveBtn.addEventListener("click", function() {
+		modal.style.display = "block"
+});
+
+buttonClose.addEventListener("click", function() {
+		modal.style.display = "none"
+});
+
+nameInput.addEventListener("input", function() {
+		text.value = "Меня зовут " + nameInput.value + ". И я хочу спросить:";
+
+		if (nameInput.value == "") {
+			text.value = ""
+		}
+});
